42d01fe98c360423c29085f2aaab9d6ecbb6b3a6
Bezzy <m.buzyarsest@gmail.com>
2023-12-25 00:38:46 +0100

    commit

--- main.js
+++ main.js
@@ -215,64 +215,9 @@ let el2 = create_element('div', {id: 'foo'},
 
 render(el, root);
 
-// function button(id, str, color) {
-//     let result = false;
-//     if (click_target.classList.contains(id)) {
-//         result = true;
-//     }
-//     let button = document.createElement("button");
-//     button.style.backgroundColor = color;
-//     button.innerText = str;
-//     button.classList.add(id);
-//     new_dom.child.push(button);
-
-//     return result;
-// }
-
-// let new_dom = document.createElement("main");
-// new_dom.setAttribute("id", "main");
-// root.appendChild(new_dom);
-// let old_dom = new_dom;
-
-// let button_visible = true;
-
-// let e_queue = [];
-// addEventListener("click", function(e) {
-//     e_queue.push(e);
-//     immediate_path();
-// });
-
-
-
-// let click_target = root;
-
-// let color = "#555";
-// function immediate_path() {
-//     while (e_queue.length > 0) {
-//         let ev = e_queue.pop();
-//         switch (ev.type) {
-//             case 'click': {
-//                 console.log("Ive been clicked");
-//                 click_target = ev.target;
-                
-//             } break;
-//             default: {
-//                 console.log("rien de particulier");
-//             }
-//         }
-//     }
-
-//     old_dom = new_dom;
-//     new_dom = document.createElement("main");
-//     new_dom.setAttribute("id", "main");
-
-    
-//     if (button_visible) {
-//         if (button(1, "Hello world", color)) {
-//             color = "#74A"
-//         }
-//     }
-
-//     root.removeChild(document.getElementById("main"));
-//     root.appendChild(new_dom);
-// }
+let color = "#555";
+function immediate_path() {
+    if (button(1, "Hello world", color))
+        color = "#74A";
+    requestAnimationFrame(() => {immediate_path()});
+}
